798e8219ee9d75a0ef8ccf2402e1e489b33731ea
Tobias Umbach <tobias@fastmail.mx>
2014-03-27 09:49:23 +0100

    Add CheckException class

---
+++ src/org/ivran/customjoin/command/CheckException.java
@@ -0,0 +1,11 @@
+package org.ivran.customjoin.command;
+
+public class CheckException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public CheckException(String message) {
+    super(message);
+  }
+
+}
