2df64c02669c82df6a7bd80d4aa57dc18d013a2d
Tobias Umbach <tobias@fastmail.mx>
2014-04-01 09:41:54 +0200

    Add detection and stripping methods for colors and formats

--- src/org/ivran/customjoin/FormatCodes.java
+++ src/org/ivran/customjoin/FormatCodes.java
@@ -7,6 +7,22 @@ public class FormatCodes {
   private static final Pattern colorCodes = Pattern.compile("(?i)&([0-9A-Fa-f])");
   private static final Pattern formatCodes = Pattern.compile("(?i)&([K-Ok-oRr])");
 
+  public static boolean containsColors(String s) {
+    return colorCodes.matcher(s).find();
+  }
+
+  public static boolean containsFormats(String s) {
+    return formatCodes.matcher(s).find();
+  }
+
+  public static String stripColors(String s) {
+    return colorCodes.matcher(s).replaceAll("");
+  }
+
+  public static String stripFormats(String s) {
+    return formatCodes.matcher(s).replaceAll("");
+  }
+
   public static String applyColors(String s) {
     return colorCodes.matcher(s).replaceAll("\247$1");
   }
