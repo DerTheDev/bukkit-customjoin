b78bc17786b0f52b7c2a8206a4de6b9cb2484240
Tobias Umbach <tobias@fastmail.mx>
2014-04-01 10:33:00 +0200

    Made the message setting executors subclasses of SetMessageBaseExecutor

--- src/org/ivran/customjoin/command/SetMsgExecutor.java
+++ src/org/ivran/customjoin/command/SetMsgExecutor.java
@@ -4,58 +4,35 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.ivran.customjoin.CustomJoinPlugin;
-import org.ivran.customjoin.FormatCodes;
-import org.ivran.customjoin.MessageType;
 import org.ivran.customjoin.R;
 
-public class SetMsgExecutor extends AbstractExecutor implements ICommandCheck {
+public class SetMsgExecutor extends SetMessageBaseExecutor {
 
   private final FileConfiguration config;
 
   public SetMsgExecutor(CustomJoinPlugin plugin) {
-    super(plugin);
+    super(plugin, 0);
 
     addCheck(new PermissionCheck("customjoin.set"));
     addCheck(new ArgumentCountCheck(1, -1));
-    addCheck(this);
 
     this.config = plugin.getConfig();
   }
 
   @Override
-  public void doCheck(CommandSender sender, Command cmd, String alias, String[] args) throws CheckException {
-    try {
-      MessageType.valueOf(args[0].toUpperCase());
-    }
-    catch (IllegalArgumentException e) {
-      throw new CheckException(R.format("Command.UnknownMessageType", args[0]));
-    }
-  }
-
-  @Override
-  protected String execute(CommandSender sender, Command cmd, String[] args) {
+  protected String createFormat(CommandSender sender, Command cmd, String[] args) {
     StringBuilder messageBuilder = new StringBuilder();
 
     for (int i = 1; i < args.length; i++) {
       messageBuilder.append(args[i]).append(' ');
     }
 
-    String message = messageBuilder.toString().trim();
+    return messageBuilder.toString().trim();
+  }
 
-    StringBuilder statusBuilder = new StringBuilder();
-    statusBuilder.append(R.get("Color.Success")).append(R.format("Command.MessageSet", args[0]));
-
-    if (!sender.hasPermission("customjoin.colors") && FormatCodes.containsColors(message)) {
-      message = FormatCodes.stripColors(message);
-      statusBuilder.insert(0, R.get("Color.Warning") + R.get("Command.MessageSet.ColorsRemoved") + '\n');
-    }
-
-    if (!sender.hasPermission("customjoin.formats") && FormatCodes.containsFormats(message)) {
-      message = FormatCodes.stripFormats(message);
-      statusBuilder.insert(0, R.get("Color.Warning") + R.get("Command.MessageSet.FormatsRemoved") + '\n');
-    }
-
-    config.set(String.format("format.%s", args[0]), message);
-    return statusBuilder.toString();
+  @Override
+  protected String saveFormat(String format, String[] args) {
+    config.set(String.format("format.%s", args[0]), format);
+    return R.get("Color.Success") + R.format("Command.MessageSet", args[0]);
   }
 }
--- src/org/ivran/customjoin/command/SetPlayerMsgExecutor.java
+++ src/org/ivran/customjoin/command/SetPlayerMsgExecutor.java
@@ -4,39 +4,24 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.ivran.customjoin.CustomJoinPlugin;
-import org.ivran.customjoin.FormatCodes;
-import org.ivran.customjoin.MessageType;
 import org.ivran.customjoin.R;
 
-public class SetPlayerMsgExecutor extends AbstractExecutor implements ICommandCheck {
+public class SetPlayerMsgExecutor extends SetMessageBaseExecutor {
 
   private final FileConfiguration config;
 
   public SetPlayerMsgExecutor(CustomJoinPlugin plugin) {
-    super(plugin);
+    super(plugin, 1);
 
     addCheck(new PermissionCheck("customjoin.setplayermessage"));
     addCheck(new ArgumentCountCheck(2, -1));
-    addCheck(this);
 
     this.config = plugin.getConfig();
   }
 
-  @Override
-  public void doCheck(CommandSender sender, Command cmd, String alias, String[] args) throws CheckException {
-    try {
-      MessageType.valueOf(args[1].toUpperCase());
-    }
-    catch (IllegalArgumentException e) {
-      throw new CheckException(R.format("Command.UnknownMessageType", args[1]));
-    }
-  }
 
   @Override
-  protected String execute(CommandSender sender, Command cmd, String[] args) {
-    String message = null;
-    StringBuilder statusBuilder = new StringBuilder();
-
+  protected String createFormat(CommandSender sender, Command cmd, String[] args) {
     if (args.length - 2 > 0) {
       StringBuilder messageBuilder = new StringBuilder();
 
@@ -44,36 +29,25 @@ public class SetPlayerMsgExecutor extends AbstractExecutor implements ICommandCh
         messageBuilder.append(args[i]).append(' ');
       }
 
-      message = messageBuilder.toString().trim();
+      return messageBuilder.toString().trim();
     }
-
-    if (!sender.hasPermission("customjoin.colors") && FormatCodes.containsColors(message)) {
-      message = FormatCodes.stripColors(message);
-
-      statusBuilder
-      .append(R.get("Color.Warning")).append(R.get("Command.MessageSet.ColorsRemoved")).append('\n');
+    else {
+      return null;
     }
+  }
 
-    if (!sender.hasPermission("customjoin.formats") && FormatCodes.containsFormats(message)) {
-      message = FormatCodes.stripFormats(message);
-
-      statusBuilder
-      .append(R.get("Color.Warning")).append(R.get("Command.MessageSet.FormatsRemoved")).append('\n');
-    }
-
+  @Override
+  protected String saveFormat(String format, String[] args) {
     String player = args[0];
     String type = args[1];
 
-    config.set(String.format("custom.%s.%s", type, player), message);
-    statusBuilder.append(R.get("Color.Success"));
+    config.set(String.format("custom.%s.%s", type, player), format);
 
-    if (message == null) {
-      statusBuilder.append(R.format("Command.PlayerMessageDeleted", player, type));
+    if (format == null) {
+      return R.get("Color.Success") + R.format("Command.PlayerMessageDeleted", player, type);
     }
     else {
-      statusBuilder.append(R.format("Command.PlayerMessageSet", player, type));
+      return R.get("Color.Success") + R.format("Command.PlayerMessageSet", player, type);
     }
-
-    return statusBuilder.toString();
   }
 }
