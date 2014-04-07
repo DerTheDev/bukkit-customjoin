dfd977abbce2a037ab47477badd9a17e6fd0eda6
Tobias Umbach <tobias@fastmail.mx>
2014-04-07 13:57:55 +0200

    Register command executors for setmyjoin and setmyquit

--- src/org/ivran/customjoin/CustomJoinPlugin.java
+++ src/org/ivran/customjoin/CustomJoinPlugin.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.ivran.customjoin.command.SetMessageExecutor;
+import org.ivran.customjoin.command.SetMyMessageExecutor;
 
 public class CustomJoinPlugin extends JavaPlugin {
 
@@ -21,6 +22,8 @@ public class CustomJoinPlugin extends JavaPlugin {
     getCommand("setjoin").setExecutor(new SetMessageExecutor(config, "format.join", "Join"));
     getCommand("setquit").setExecutor(new SetMessageExecutor(config, "format.quit", "Quit"));
     getCommand("setkick").setExecutor(new SetMessageExecutor(config, "format.kick", "Kick"));
+    getCommand("setmyjoin").setExecutor(new SetMyMessageExecutor(config, "Join"));
+    getCommand("setmyquit").setExecutor(new SetMyMessageExecutor(config, "Quit"));
 
     getServer().getPluginManager().registerEvents(new JoinLeaveListener(config), this);
     getLogger().info(formatString("Plugin.Enabled", pdf.getName(), pdf.getVersion()));
