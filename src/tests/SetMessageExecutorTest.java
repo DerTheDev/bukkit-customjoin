a82cc2ac3f0c48ce989053078a99a26267c81c3d
Tobias Umbach <tobias@fastmail.mx>
2014-04-07 13:58:54 +0200

    Specify wildcard imports in SetMessageExecutorTest

--- src/tests/SetMessageExecutorTest.java
+++ src/tests/SetMessageExecutorTest.java
@@ -1,10 +1,14 @@
 package tests;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
-import static org.ivran.customjoin.ResourceHelper.getString;
 import static org.ivran.customjoin.ResourceHelper.formatString;
+import static org.ivran.customjoin.ResourceHelper.getString;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Collection;
