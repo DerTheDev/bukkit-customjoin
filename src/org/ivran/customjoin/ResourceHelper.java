1cdd77f17a18773deb1927612ddc2b9d164d8228
Tobias Umbach <tobias@fastmail.mx>
2014-04-09 12:30:05 +0200

    Add getColor method to ResourceHelper

--- src/org/ivran/customjoin/ResourceHelper.java
+++ src/org/ivran/customjoin/ResourceHelper.java
@@ -2,12 +2,18 @@ package org.ivran.customjoin;
 
 import java.util.ResourceBundle;
 
+import org.bukkit.ChatColor;
+
 public class ResourceHelper {
 
   private static final ResourceBundle bundle = ResourceBundle.getBundle("MessagesBundle");
 
   public static String formatMessage(String key, Object... args) {
     return String.format(getMessage(key), args);
+  }
+
+  public static ChatColor getColor(String key) {
+    return ChatColor.getByChar(bundle.getString("Color." + key));
   }
 
   public static String getMessage(String key) {
